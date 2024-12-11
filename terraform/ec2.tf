6eef64ceeecf1c48eec0a848f26b0f8b52549735
Afeez-AA <afeeadeboyee@gmail.com>
2024-12-11 17:42:16 +0000

    fixed ami az issue

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,8 +1,11 @@
+data "aws_availability_zones" "available" {}
+
 resource "aws_instance" "ec2" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
   key_name               = var.key_pair_name
   subnet_id              = aws_subnet.public_subnet.id
+  availability_zone      = data.aws_availability_zones.available.names[0]
   vpc_security_group_ids = [aws_security_group.sg.id]
   tags = {
 
